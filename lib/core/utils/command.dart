55547442b1a71b6c746e5bb5699142c7ce34d7b5
Tomas Baran <tomas.baran@gmail.com>
2025-08-13 16:44:04 +0200

    fix: Adapt Command to my architecture to work with Result rather than with thrown errors

--- lib/core/utils/command.dart
+++ lib/core/utils/command.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:persistent_storage_key_value/core/utils/result.dart';
 
 part 'command.freezed.dart';
 
@@ -12,8 +13,8 @@ sealed class CommandState<T> with _$CommandState<T> {
 }
 
 class Command<T> {
-  final Future<T> Function() _execute;
-  Command({required Future<T> Function() execute}) : _execute = execute;
+  final Future<Result<T>> Function() _execute;
+  Command({required Future<Result<T>> Function() execute}) : _execute = execute;
 
   final _state = ValueNotifier<CommandState<T>>(CommandState.idle());
   get state => _state.value;
@@ -21,11 +22,11 @@ class Command<T> {
   execute() async {
     if (state is Executing<T>) return;
     _state.value = CommandState.executing();
-    try {
-      final result = await _execute();
-      _state.value = CommandState.succeeded(result);
-    } catch (e) {
-      _state.value = CommandState.failed('Command failed:$e');
-    }
+
+    final result = await _execute();
+    result.when(
+      success: (value) => _state.value = CommandState.succeeded(value),
+      error: (message) => _state.value = CommandState.failed(message),
+    );
   }
 }
