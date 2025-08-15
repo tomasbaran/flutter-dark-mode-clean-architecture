e1ef1c495bd7203d8b5e50c38af07e89295b9def
Tomas Baran <tomas.baran@gmail.com>
2025-08-15 16:19:59 +0200

    Add mockito and generator setup

---
+++ test/core/mocks/mocks_generator.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:persistent_storage_key_value/features/theme/domain/repos/theme_repo.dart';
+
+@GenerateNiceMocks([MockSpec<ThemeRepo>()])
+void main() {}
