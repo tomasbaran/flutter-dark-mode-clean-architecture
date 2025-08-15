e9f1a354f55fe745a0c1b3d676276525425a0b92
Tomas Baran <tomas.baran@gmail.com>
2025-08-15 16:20:29 +0200

    Add VM test

--- test/features/theme/presentation/vm/theme_vm_test.dart
+++ test/features/theme/presentation/vm/theme_vm_test.dart
@@ -1,31 +1,55 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:persistent_storage_key_value/core/app_config.dart';
+import 'package:persistent_storage_key_value/core/utils/command.dart';
 import 'package:persistent_storage_key_value/core/utils/result.dart';
-import 'package:persistent_storage_key_value/features/theme/data/repos/theme_repo_fake_impl.dart';
 import 'package:persistent_storage_key_value/features/theme/domain/entities/theme_entity.dart';
-import 'package:persistent_storage_key_value/features/theme/domain/repos/theme_repo.dart';
+import 'package:persistent_storage_key_value/features/theme/presentation/theme_vm.dart';
+
+import '../../../../core/mocks/mocks_generator.mocks.dart';
 
 void main() {
-  late ThemeRepo themeRepo;
-  late ThemeVM vm;
+  late MockThemeRepo mockThemeRepo;
+  late ThemeVM sut;
   group('ThemeVM', () {
     setUp(() {
-      themeRepo = ThemeRepoFakeImpl();
-      vm = ThemeVM(themeRepo);
+      mockThemeRepo = MockThemeRepo();
+      sut = ThemeVM(themeRepo: mockThemeRepo);
+      provideDummy(Result.success(AppConfig.defaultThemeMode));
     });
     test('should return the default theme mode by default', () async {
-      final result = await vm.getThemeMode();
-      final expectedResult = Result.success(AppConfig.defaultThemeMode);
-      expect(result, expectedResult);
+      // Arrange
+      when(
+        mockThemeRepo.getThemeMode(),
+      ).thenAnswer((_) async => Result.success(AppConfig.defaultThemeMode));
+      // Act
+      await sut.init();
+      // Assert
+      verify(mockThemeRepo.getThemeMode());
+      final expectedResult = AppConfig.defaultThemeMode;
+      expect(
+        sut.loadResourcesCommand.state,
+        CommandState.succeeded(expectedResult),
+      );
     });
 
     test('should return the correct theme mode after setting it', () async {
       for (final mode in AppThemeMode.values) {
-        await vm.setThemeMode(mode);
-        final result = await vm.getThemeMode();
-        final expectedResult = Result.success(mode);
-        expect(result, expectedResult);
-        expect(vm.command.value, isA<Succeeded<AppThemeMode>>());
+        // Arrange
+        when(
+          mockThemeRepo.getThemeMode(),
+        ).thenAnswer((_) async => Result.success(mode));
+
+        // Act
+        await sut.setThemeMode(mode);
+
+        // Assert
+        verify(mockThemeRepo.setThemeMode(mode));
+        final expectedResult = mode;
+        expect(
+          sut.setThemeCommand.state,
+          CommandState.succeeded(expectedResult),
+        );
       }
     });
   });
