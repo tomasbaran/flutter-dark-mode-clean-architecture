8a961fee0462ef7a8ae5e41000a301a109de2b94
Tomas Baran <tomas.baran@gmail.com>
2025-09-02 14:05:38 -0600

    feat: Add key_value_store_impl

--- lib/core/storage/key_value_store.dart
+++ lib/core/storage/key_value_store.dart
@@ -1,4 +1,4 @@
 abstract class KeyValueStore {
   Future<bool> writeString(String key, String value);
-  Future<String> readString(String key);
+  Future<String?> readString(String key);
 }
---
+++ lib/core/storage/key_value_store_impl.dart
@@ -0,0 +1,27 @@
+import 'package:persistent_storage_key_value/core/storage/key_value_store.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class KeyValueStoreImpl implements KeyValueStore {
+  SharedPreferences? _prefs;
+  get _isInitialized => _prefs != null;
+
+  Future<void> _init() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  Future<void> _ensureInitialized() async {
+    if (!_isInitialized) await _init();
+  }
+
+  @override
+  Future<bool> writeString(String key, String value) async {
+    await _ensureInitialized();
+    return await _prefs!.setString(key, value);
+  }
+
+  @override
+  Future<String?> readString(String key) async {
+    await _ensureInitialized();
+    return _prefs!.getString(key);
+  }
+}
