4fe5d0fde2c3adefc94d648e8572cb0ea5bfebb3
Tomas Baran <tomas.baran@gmail.com>
2025-09-12 20:09:40 -0600

    feat: Add snack bar when error occurs in the app loading wrapper

--- lib/core/widgets/app_loading_wrapper.dart
+++ lib/core/widgets/app_loading_wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_storage_key_value/core/utils/command.dart';
 
-class AppLoadingWrapper<T> extends StatelessWidget {
+class AppLoadingWrapper<T> extends StatefulWidget {
   final Widget child;
   final ValueNotifier<CommandState<T>> loadingState;
   const AppLoadingWrapper({
@@ -11,20 +11,51 @@ class AppLoadingWrapper<T> extends StatelessWidget {
   });
 
   @override
+  State<AppLoadingWrapper<T>> createState() => _AppLoadingWrapperState<T>();
+}
+
+class _AppLoadingWrapperState<T> extends State<AppLoadingWrapper<T>> {
+  @override
+  void initState() {
+    super.initState();
+    widget.loadingState.addListener(_onError);
+  }
+
+  @override
+  void dispose() {
+    widget.loadingState.removeListener(_onError);
+    super.dispose();
+  }
+
+  void _onError() {
+    final state = widget.loadingState.value;
+    if (state is Failed<T>) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(state.message)),
+        );
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
-      valueListenable: loadingState,
-      builder: (_, loadingState, __) => (loadingState is Executing<T>)
-          ? Stack(
-              children: [
-                child,
-                Container(
-                  color: Colors.white,
-                  child: const Center(child: CircularProgressIndicator()),
-                ),
-              ],
-            )
-          : child,
+      valueListenable: widget.loadingState,
+      builder: (_, loadingState, __) {
+        if (loadingState is Executing<T>) {
+          return Stack(
+            children: [
+              widget.child,
+              Container(
+                color: Colors.white,
+                child: const Center(child: CircularProgressIndicator()),
+              ),
+            ],
+          );
+        }
+        return widget.child;
+      },
     );
   }
 }
